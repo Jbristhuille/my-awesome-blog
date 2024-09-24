5c9db695368150f8bf3127885599dd64cb3229c1
Jbristhuille <jbristhuille@gmail.com>
2024-09-24 14:52:42 +0200

    Create admin page, auto redirect to admin/articles page

---
+++ app/admin/page.tsx
@@ -0,0 +1,18 @@
+/******************************************************************************
+ * @Author                : Jbristhuille<jbristhuille@gmail.com>              *
+ * @CreatedDate           : 2024-09-24 14:51:54                               *
+ * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>              *
+ * @LastEditDate          : 2024-09-24 14:52:09                               *
+ *****************************************************************************/
+
+/* SUMMARY
+  * Imports
+*/
+
+/* Imports */
+import { redirect } from "next/navigation";
+/***/
+
+export default function Page() {
+  redirect("/admin/articles");
+}
