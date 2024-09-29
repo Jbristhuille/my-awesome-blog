5ff100fee757f1f1721d3b90dc3477732a9c32bf
Jbristhuille <jbristhuille@gmail.com>
2024-09-29 19:59:55 +0200

    Apply minimal style on button

--- app/ui/Button.tsx
+++ app/ui/Button.tsx
@@ -2,25 +2,30 @@
  * @Author                : Jbristhuille<jbristhuille@gmail.com>              *
  * @CreatedDate           : 2024-09-24 14:14:02                               *
  * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>              *
- * @LastEditDate          : 2024-09-24 14:32:20                               *
+ * @LastEditDate          : 2024-09-29 19:55:23                               *
  *****************************************************************************/
 
 /* SUMMARY
   * Interfaces
+  * Styles
 */
 
 "use client";
 
 /* Interfaces */
 interface IPropsButton {
-  callback: (e: any)=>{},
+  callback: (e: any)=>void,
   content: string
 }
 /***/
 
+/* Styles */
+import styles from "@/app/ui/Button.module.scss";
+/***/
+
 export default function Button(props: IPropsButton) {
   return(
-    <button onClick={(e) => props.callback({})}>
+    <button className={styles.button} onClick={(e) => props.callback({})}>
       {props.content}
     </button>
   );
